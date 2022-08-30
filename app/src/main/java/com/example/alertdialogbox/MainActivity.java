54a4b990430dbc9774815c801c818de7d17dc554
Haseeb Malik <haseebmalik.info@gmail.com>
2022-08-30 14:31:35 +0500

    second commit

--- app/src/main/java/com/example/alertdialogbox/MainActivity.java
+++ app/src/main/java/com/example/alertdialogbox/MainActivity.java
@@ -5,47 +5,64 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button alert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        alert = findViewById(R.id.alert);
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-
-        alertDialog.setTitle("Terms and Conditions");
-
-        alertDialog.setIcon(R.drawable.ic_baseline_priority_high_24);
-
-        alertDialog.setMessage("Have you read all terms and conditions");
-
-        alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+        alert.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(MainActivity.this, "deleted", Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
 
+
+                alertDialog.setTitle("Terms and Conditions");
+
+                alertDialog.setIcon(R.drawable.ic_baseline_priority_high_24);
+
+                alertDialog.setMessage("Have you read all terms and conditions");
+
+                alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                        Toast.makeText(MainActivity.this, "deleted", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+                alertDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                        Toast.makeText(MainActivity.this, "cancel", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+                alertDialog.setNeutralButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                        Toast.makeText(MainActivity.this, "back", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                alertDialog.show();
             }
         });
 
-        alertDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(MainActivity.this, "cancel", Toast.LENGTH_SHORT).show();
 
-            }
-        });
 
-        alertDialog.setNeutralButton("cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(MainActivity.this, "back", Toast.LENGTH_SHORT).show();
-            }
-        });
 
-        alertDialog.show();
 
 
     }
